69d9c0057e49dd8610b276cf4f9b1f7124c11bf8
neledigital <fabianalbertovidalroa@gmail.com>
2021-11-30 04:28:01 -0500

    color purple

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -11,9 +11,9 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Red Social',
+      title: 'Red Social Lore Anime',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
       initialRoute: '/',
       routes: {
